3aedfcc8f8fffbec900fbd1c4cede8347959e91f
MaksymukNatalia <146457298+MaksymukNatalia@users.noreply.github.com>
2024-05-20 22:40:25 +0200

    Create autorefresh_instagram.sh

---
+++ autorefresh_instagram.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+path="/etc/environment"
+
+
+currentToken=$(grep '^INSTAGRAM_TOKEN=' "$path" | cut -d'=' -f2 | tr -d '"')
+
+
+newToken=$(curl -s "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${currentToken}" | grep -o '"access_token":"[^"]*' | cut -d'"' -f4)
+
+
+if [ -z "$newToken" ]; then
+  echo "Failed to retrieve new access token."
+  exit 1
+fi
+
+
+awk -v newToken="${newToken}" '/^INSTAGRAM_TOKEN=/{gsub(/=.*/, "=" newToken)}1' "$path" > tmpfile && mv tmpfile "$path"
+
+
+echo "New INSTAGRAM_TOKEN: $newToken"
+
+
+echo "OLD token: $currentToken"
